2b0d003f95f8efb16aac1a36507bc263f7fe6776
Reza Ghiasi <imrezaghiasi@gmail.com>
2022-07-14 21:02:58 +0430

    Add Register

---
+++ IdentityApp/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using IdentityApp.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    EmailConfirmed = true
+                };
+              var result = await _userManager.CreateAsync(user, model.Password);
+
+              if (result.Succeeded)
+                  return RedirectToAction("Index", "Home");
+
+              foreach (var error in result.Errors)
+              {
+                  ModelState.AddModelError("",error.Description);
+              }
+
+            }
+            return View(model);
+        }
+    }
+}
---
+++ IdentityApp/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.ViewModels.Account;
+
+public class RegisterViewModel
+{
+    [Required]
+    [Display(Name = "نام کاربری")]
+    public string UserName { get; set; }
+    [Required]
+    [Display(Name = "ایمیل")]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [Display(Name = "رمز عبور")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [Required]
+    [Display(Name = "تکرار رمز عبور")]
+    [Compare(nameof(Password))]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; }
+}
