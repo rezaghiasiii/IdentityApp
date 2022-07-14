6cc99435edb484a720ec7a27fedaef37eabe2687
Reza Ghiasi <imrezaghiasi@gmail.com>
2022-07-14 19:57:35 +0430

    Add project files.

---
+++ IdentityApp/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.Models;
+
+public class Employee
+{
+    [Key]
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+    public string Gender { get; set; }
+}
